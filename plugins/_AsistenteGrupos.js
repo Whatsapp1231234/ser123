25fb92416b6724c150d6e8e32f20720ab4741605
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-05-23 12:00:19 +0300

    ыват

--- plugins/_AsistenteGrupos.js
+++ plugins/_AsistenteGrupos.js
@@ -32,42 +32,26 @@ Por cierto soy un asistente virtual en este grupo|^para más detalles invoca a u
 await conn.reply(m.chat, text, m, { mentions: this.parseMention(text) })
 } 
 if (/Otakus Together$/i.test(m.text) && chat.asistente && !chat.isBanned) {
-  m.reply(`ㄖㄒ卂Ҡ凵丂   ㄒㄖᎶ乇ㄒ卄乇尺`)
+  m.reply(``)
 } 
 if (/Otakus TogetherS$/i.test(m.text) && chat.asistente && !chat.isBanned) {
-  m.reply(`🍓⃢⃤ᬽㄖㄒ卂Ҡ凵丂  ㄒㄖᎶ乇ㄒ卄乇尺🍜⃢⃟ᭀᬽ`)
+  m.reply(``)
 } 
 if (/^otakus forever$/i.test(m.text) && chat.asistente && !chat.isBanned) {
-  m.reply(`🍓⃢⃤ᬽㄖㄒ卂Ҡ凵丂  千ㄖ尺乇ᐯ乇尺🍜⃢⃟ᭀᬽ`)
+  m.reply(``)
 } 
 if (/nombre del grupo de sailor moon$/i.test(m.text) && chat.asistente && !chat.isBanned) {
-  m.reply(`❤️𝓕𝓪𝓷 𝓭𝓮 𝓢𝓪𝓲𝓵𝓸𝓻 𝓜𝓸𝓸𝓷🌙💖`)
+  m.reply(``)
 } 
 if (/muestra el grupo de aportes$/i.test(m.text) && chat.asistente && !chat.isBanned) {
   m.reply(`Se les invita al grupo de aportes de
-🍓⃢⃤ᬽㄖㄒ卂Ҡ凵丂  
-ㄒㄖᎶ乇ㄒ卄乇尺🍜⃢⃟ᭀᬽ 
-**Para estar en ese grupo de aportes no es necesario tener aportes sin embargo es un grupo 0 chat por lo que por respeto a los aportadores oficiales está en modo administrador**
 
-Quien quiera pertenecer a ese grupo y tenga aportes me dice para prestarle admin porque no se puede chatear en aquel grupo por respeto a los aportadores oficiales
-
-https://chat.whatsapp.com/DhvxhmZ4lMkLppU0obHWp4
-
-Se puede aportar hasta el momento cualquier tipo de contenido incluyendo películas que no tengan relación con anime...
-
-O música y manga
-
-Cómo requisito para permanecer en ese grupo tienen que estar en este grupo...
-
-*Quién se salga de este grupo por automático será expulsado del grupo de aportes*`)
+*`)
 } 
 if (/grupo de aportes forever$/i.test(m.text) && chat.asistente && !chat.isBanned) {
   m.reply(`Este es el grupo de aportes de
-🍓⃢⃤ᬽㄖㄒ卂Ҡ凵丂  千ㄖ尺乇ᐯ乇尺🍜⃢⃟ᭀᬽ
 
-https://chat.whatsapp.com/DhvxhmZ4lMkLppU0obHWp4
 
-⚡Este grupo es *No Chat*⚡
 
 *Permitido*✅
 
@@ -98,60 +82,18 @@ if (/Reglas de otakus forever anti$/i.test(m.text) && chat.asistente && !chat.is
 *ㄖㄒ卂Ҡ凵丂  千ㄖ尺乇ᐯ乇尺*
 *(Antibinarios)*
 
-*Reglas del grupo*
-*[1]Pasar links de grupos esta prohibido*
 
-*[2]Queda estrictamente prohibido el Porno y el Ecchi, Hentai, para este último tenemos grupos dedicados*
 
-*[3]Está prohibido agregar administradores sin previo aviso, info para misiones* 
-         
-*[4]Tienen prohibido faltar al respeto a menos de que se estén llevando*
-
-*[5]El spam de stickers y de texto esta permitido*
-
-*La Nueva generación forever*
-
-https://facebook.com/groups/849679409107132`)
+`)
 } 
 if (/enlace de invitación|link|enlace del grupo$/i.test(m.text) && chat.asistente && !chat.isBanned) {
   let text = `Solo tienes dos opciones para llegar al grupo principal @${who.split("@s.whatsapp.net")[0]}
 
-ℂ𝕒𝕗𝕖𝕔𝕚𝕥𝕠 ℍ𝕠𝕣𝕚-𝕊𝕒𝕟𝕕𝕚𝕒🍉☕🥢
-https://chat.whatsapp.com/H0SheP7ippc1dF9uxL04Gt
-
 o
 
-しᝪᗷᗷᎩ de 
-ㄖㄒ卂Ҡ凵丂   ㄒㄖᎶ乇ㄒ卄乇尺
-https://chat.whatsapp.com/L4VRAzaYc11D4LSpt8rB9W
+ *вау*:
 
-Ahí se les realizará una entrevista dónde tendrán que responder las siguientes preguntas
-
-*ɴᴏᴍʙʀᴇ*:
-
-
- *ᴇᴅᴀᴅ*:
-
-
- *ᴘᴀÍꜱ* :
-
-
- *ᴡᴀɪꜰᴜ ᴏ ʜᴜsʙᴀɴᴅᴏ*:
-
-
- *ᴀɴɪᴍᴇ ꜰᴀᴠᴏʀɪᴛᴏ*: 
-
-
-  *ᴍᴀɴɢᴀ ꜰᴀᴠᴏʀɪᴛᴏ* :
-
-
- *ᴅᴇꜱᴅᴇ ʜᴀᴄᴇ ᴄᴜÁɴᴛᴏ ᴇʀᴇꜱ ᴏᴛᴀᴋᴜ*:
-
-
- *ꜰᴏᴛᴏ o ᴍᴇɴsᴀᴊᴇ ᴅᴇ ᴠᴏᴢ*:
-
-
-**TODOS ESTOS DATOS PUEDEN SER EN PRIVADO SI QUIEREN CON ALGUNO DE LOS ADMINS ACTIVOS**`
+**Это что такое еще**`
 await conn.reply(m.chat, text, m, { mentions: this.parseMention(text) })
 }    
 /*if (/^.jadibot|^*jadibot|^#jadibot|^/jadibot|^serbot$/i.test(m.text) && chat.asistente && !chat.isBanned) {
